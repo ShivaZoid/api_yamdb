5dc92ee6a79bdcdb19d226c8628cf97a83993b30
ShivaZoid <krovvar14@yandex.ru>
2022-12-26 22:42:38 +0800

    Добавление в админку User

--- api_yamdb/yamdb/admin.py
+++ api_yamdb/yamdb/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Конфигурация отображения данных.
+
+    Attributes:
+        list_display: отображаемые поля.
+        search_fields: интерфейс для поиска по тексту постов.
+        list_filter: возможность фильтрации по дате.
+    """
+
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'bio',
+        'role',
+    )
+    search_fields = ('username', 'role',)
+    list_filter = ('username',)
+    empty_value_display = '-пусто-'
